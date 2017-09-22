d3c891ca702b22f5e2549d758d16064af50b981a
jyhou <houjiangyi327@gmail.com>
2017-09-21 22:53:00 -0400

    ""

--- LeetCodeHou/src/P53_MaximumSubarray/Solution.java
+++ LeetCodeHou/src/P53_MaximumSubarray/Solution.java
@@ -14,13 +14,14 @@ public class Solution {
 		int max = 0;
 		int sum = 0;
 		if (nums.length < 1) return max;
+		max = nums[0];
 		for (int i = 0; i < nums.length; i++) {
 			sum = sum + nums[i];
 			if (sum >= 0) {
 				max = (max < sum) ? sum : max;
-			} else {
-				sum = 0;
+			} else {			
 				max = (max < sum) ? sum : max;
+				sum = 0;
 			}
 		}
 		
