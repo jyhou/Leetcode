61fab5751de4e2a782e9993d637c293156c78053
jyhou <houjiangyi327@gmail.com>
2017-09-21 22:27:48 -0400

    ""

---
+++ LeetCodeHou/src/P53_MaximumSubarray/Test.java
@@ -0,0 +1,15 @@
+package P53_MaximumSubarray;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Solution solve = new Solution();
+		
+		int[] nums1 = {-2,1,-3,4,-1,2,1,-5,4};
+		
+		System.out.printf("num1: %s\n", nums1.toString());
+		
+	}
+
+}
